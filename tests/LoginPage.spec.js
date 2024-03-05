3df0398622c1d254541670378117f2273ffbb2c1
grimripper <inggyovannycavazos@gmail.com>
2024-03-05 14:31:19 -0800

    WTF this file is already a part of the project

--- tests/LoginPage.spec.js
+++ tests/LoginPage.spec.js
@@ -3,15 +3,20 @@ const{test, expect} = require('@playwright/test')
 test('Login test', async({browser})=>{
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator("#username");
+    const singin = page.locator("#signInBtn");
 
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
     console.log(await page.title());
 
-    await page.locator('#username').fill('rahulshetty');
+    await userName.fill('rahulshetty');
     await page.locator('[type="password"]').fill('learning');
-    await page.locator("#signInBtn").click();
+    await singin.click();
 
     console.log(await page.locator("[style *= 'block']").textContent());
 
-    await expect(page.locator("[style *= 'block']")).toContainText("Incorrect")
+    await expect(page.locator("[style *= 'block']")).toContainText("Incorrect");
+
+    await userName.fill("").fill("rahulshettyacademy");
+    await singin.click();
 });
